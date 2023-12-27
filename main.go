760eaa8e1c162f6c2be0c8679f0a2663f1e83524
Teodoro <tedtke514@gmail.com>
2023-12-27 17:40:04 -0600

    initial logic

--- main.go
+++ main.go
@@ -3,6 +3,31 @@ package main
 import  "fmt" 
 
 func main() {
-	fmt.Println("Welcome to our conference booking application")
+	conferenceName := "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets uint = 50 
+
+	fmt.Printf("Welcome to %v booking application.\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+	// ask user for a name
+
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter your how many tickets you want: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v.\n", firstName, lastName, userTickets, email)
 }
