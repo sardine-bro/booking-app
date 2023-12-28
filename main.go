bc4c62d294f265ddf57b292bdd42234d79e60d20
Teodoro <tedtke514@gmail.com>
2023-12-27 18:00:13 -0600

    updated go app, left on slices

--- main.go
+++ main.go
@@ -11,10 +11,12 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
+	var bookings [50]string
+	
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
 	// ask user for a name
 
 	fmt.Println("Enter your first name: ")
@@ -29,5 +31,14 @@ func main() {
 	fmt.Println("Enter your how many tickets you want: ")
 	fmt.Scan(&userTickets)
 
+	bookings[0] = firstName + " " + lastName
+	remainingTickets -= userTickets
+
+	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("The type of the array: %T\n", bookings)
+	fmt.Printf("The size of the array: %v\n", len(bookings))
+
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v.\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v.\n", remainingTickets, conferenceName)
 }
